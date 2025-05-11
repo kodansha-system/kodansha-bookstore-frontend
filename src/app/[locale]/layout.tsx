da1f19e2e4dbb1bded6d4e05996d327391298438
luongminhanh <luongminhanh.haui@gmail.com>
2025-05-11 21:39:23 +0700

    update website's name

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -17,7 +17,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Shelfly",
+  title: "Kodansha",
   description: "An online book store built with NextJS",
 };
 
